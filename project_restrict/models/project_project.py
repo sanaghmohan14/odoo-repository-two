fbd70224968086a2f20dcd7af687c9f491311141
sanagh <sanaghsn1419@gmail.com>
2026-07-27 11:28:02 +0530

    updated custom addons

--- project_restrict/models/project_project.py
+++ project_restrict/models/project_project.py
@@ -15,14 +15,15 @@ class ProjectProject(models.Model):
 
     @api.onchange('tag_ids')
     def _onchange_tag_ids(self):
+        """it is used to change the user ids when tag ids is changed"""
         if not self.tag_ids:
             return
-        users = self.env['res.users'].search([('skill_types', 'in', self.tag_ids.ids)], limit=1)
+        users = self.env['res.users'].search([('skill_types', 'in', self.tag_ids.ids)])
+        print(users.mapped("name"))
+
 
         if users:
             self.user_ids=users
-
-
 
 
 
@@ -39,34 +40,39 @@ class ProjectProject(models.Model):
                     print("inprogress is ok")
 
                     already=self.env["account.analytic.line"].search([
-                        ('task_id','=',task.id),("employee_id","=",task.user_ids.employee_id.id)], limit=1)
+                        ('task_id','=',task.id),("employee_id","=",task.user_ids.employee_id.id)])
 
                     print(already)
+
+                    user=task.user_ids[:1]
 
                     if not already:
                         self.env['account.analytic.line'].create({
                             "name": task.name,
                             "task_id": task.id,
-                            "employee_id":task.user_ids.employee_id.id,
+                            # "employee_id":task.user_ids.employee_id.id,
+                            "user_id": user.id,
+                            "employee_id":user.employee_id.id,
                             "date":fields.Date.today(),
                             "unit_amount":0
                         })
+
+                if task.stage_id.name == "Done":
+                    print("done is ok")
+
+                    total_hours = sum(task.timesheet_ids.mapped("unit_amount"))
+                            # total_hours=sum(task.unit_amount)
+                            # if task.unit_amount<=0:
+
+                    if total_hours <= 0:
+                        raise ValidationError("less than zero")
+
         return result
 
 
 
 
 
-    # def write(self,vals):
-    #     result=super().write(vals)
-    #     if "stage_id" in vals:
-    #         for task in self:
-    #             if task.stage_id.name=="Done":
-    #                 total_hours=sum(task.timesheet_ids.mapped("unit_amount"))
-    #
-    #                 if total_hours<=0:
-    #                     raise ValidationError("less than zero")
-    #     return result
 
 
 
